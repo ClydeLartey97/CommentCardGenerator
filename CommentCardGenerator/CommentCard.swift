f6b9d9b478f6fb19a13031b2bd1f46f2170b4efd
Clyde Lartey <lartey.d@etoncollege.org.uk>
2022-02-08 11:10:59 +0000

    Content view modification

---
+++ CommentCardGenerator/CommentCard.swift
@@ -0,0 +1,24 @@
+//
+//  CommentCard.swift
+//  CommentCardGenerator
+//
+//  Created by Lartey, David (HWTA) on 08/02/2022.
+//
+
+import Foundation
+
+class CommentCard {
+    var happiness: String
+    var difficulty: Int
+    var achievements: Int
+    
+    
+    
+    init () {
+        happiness = "Very Happy"
+        difficulty = 0
+        achievements = 10
+    }
+
+    
+}
--- CommentCardGenerator/ContentView.swift
+++ CommentCardGenerator/ContentView.swift
@@ -9,8 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Create Comment Card")
+            
+                .padding(30)
+            Text("Enter Level of difficulty")
+            Button("Very Hard", action: {
+            
+                
+            })
+            Button("Hard", action: {
+                
+            })
+            Button("Ok", action: {
+                
+            })
+            Button("Easy", action: {
+                
+            })
+            Button("Very Easy", action: {
+                
+            })
+
+        }
+        
+        
+        //Text("I find ")
     }
 }
 
